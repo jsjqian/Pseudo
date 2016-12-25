445981e36899be587e97ff8196a3d7e43df009dc
jsjqian <jack.qian97@gmail.com>
2016-12-25 17:24:49 -0500

    Start translating code

---
+++ pseudo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import token
+import sys
+
+def main():
+  if len(sys.argv) > 2:
+    print "pseudo: " + " ".join(sys.argv) + ". Too many arguments. Usage: pseudo file"
+    exit()
+  elif len(sys.argv) < 2:
+    print "pseudo: Too few arguments. Usage: pseudo file"
+    exit()
+  filename = sys.argv[1]
+  f = open(filename, 'r')
+  str = f.read()
+  token.tokenize(str)
+
+if __name__ == "__main__":
+  main()
---
+++ translate.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+def lookahead(tokens):
+  return [tokens.pop(0), tokens]
+
+def translate_variable(tokens):
+
+def translate_print(tokens):
+
+def translate(tokens):
