19743a6ec96f1a1beca7f50a9003ca01f1a0a5d3
jsjqian <jack.qian97@gmail.com>
2017-01-16 02:25:47 -0500

    Test assignment

--- translate.py
+++ translate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 def lookahead(tokens):
-  remain = tokens
+  remain = tokens[:]
   first = remain.pop(0)
   return [first, remain]
 
@@ -15,10 +15,57 @@ def parse_if(tokens):
   pass
 
 def parse_assign(tokens):
-  pass
+
+  res = ""
+
+  la = lookahead(tokens)
+
+  if la[0] == 'SET':
+    la = lookahead(la[1])
+    if la[0] == 'VARIABLE':
+      la = lookahead(la[1])
+      res = res + la[0] + ' = '
+      la = lookahead(la[1])
+      if la[0] == 'TO':
+        pe = parse_expression(la[1])
+        res = res + pe
+        return res
+      else:
+        raise SyntaxError('assignment error')
+    else:
+      raise SyntaxError('assignment error')
+  else:
+    raise SyntaxError('not an assignment')
 
 def parse_print(tokens):
   
+  res = "print "
+
+  pe = parse_expression(tokens)
+  res = res + pe
+
+  return res
+  
+def parse_function(tokens):
+  pass
+
+def parse_string(tokens):
+
+  res = ""
+
+  la = lookahead(tokens)
+
+  if la[0] == 'STRING':
+    string = lookahead(la[1])
+    res = res + string[0]
+    tokens = string[1]
+    return [res, tokens]
+  else:
+    raise SyntaxError('not a string')
+  
+
+def parse_expression(tokens):
+
   res = ""
 
   while len(tokens) > 0:
@@ -26,23 +73,15 @@ def parse_print(tokens):
     la = lookahead(tokens)
 
     if la[0] == 'STRING':
-      string = lookahead(la[1])
-      res = res + string[0]
-      tokens = string[1]
+      ps = parse_string(tokens)
+      res = res + ps[0]
+      tokens = ps[1]
     elif la[0] == "CALL":
       pass
     else:
-      pe = parse_expression(tokens)
-      res = res + pe[0]
-      tokens = pe[1]
+      raise SyntaxError('invalid expression')
 
   return res
-  
-def parse_function(tokens):
-  pass
-
-def parse_expression(tokens):
-  pass
 
 def parse(tokens):
 
@@ -58,7 +97,6 @@ def parse(tokens):
 
     if la[0] == "OUTPUT":
 
-      line = line + "print "
       print_statement = parse_print(la[1])
       line = line + print_statement
       res = res + line + '\n'
