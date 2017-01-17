3d76ec278567a185ed7b5c4480d1cc20a9f4ef58
jsjqian <jack.qian97@gmail.com>
2017-01-16 22:28:49 -0500

    Add variable support

--- foobar.py
+++ foobar.py
@@ -1,2 +1,5 @@
 print "Hello World!"
 print "This is a Pseudo program!"
+FOO = "bar"
+print "The value of foo was:"
+print FOO
--- translate.py
+++ translate.py
@@ -5,9 +5,6 @@ def lookahead(tokens):
   first = remain.pop(0)
   return [first, remain]
 
-def parse_declaration(tokens):
-  pass
-
 def parse_while(tokens):
   pass
 
@@ -20,22 +17,28 @@ def parse_assign(tokens):
 
   la = lookahead(tokens)
 
-  if la[0] == 'SET':
+  if la[0] == 'VARIABLE':
     la = lookahead(la[1])
-    if la[0] == 'VARIABLE':
-      la = lookahead(la[1])
-      res = res + la[0] + ' = '
-      la = lookahead(la[1])
-      if la[0] == 'TO':
-        pe = parse_expression(la[1])
-        res = res + pe
-        return res
-      else:
-        raise SyntaxError('assignment error')
+    res = res + la[0] + ' = '
+    la = lookahead(la[1])
+    if la[0] == 'TO':
+      pe = parse_expression(la[1])
+      res = res + pe
+      return res
     else:
       raise SyntaxError('assignment error')
   else:
     raise SyntaxError('not an assignment')
+
+def parse_variable(tokens):
+  
+  res = ""
+
+  la = lookahead(tokens)
+  res = res + la[0]
+  tokens = la[1]
+  return [res, tokens]
+
 
 def parse_print(tokens):
   
@@ -47,7 +50,53 @@ def parse_print(tokens):
   return res
   
 def parse_function(tokens):
-  pass
+
+  res = "def "
+
+  la = lookahead(tokens)
+
+  if la[0] == 'FUNCTION':
+    la = lookahead(la[1])
+    res = res + la[0]
+  else:
+    raise SyntaxError('improper function declaration')
+
+  la = lookahead(la[1])
+
+  if la[0] == 'TAKING':
+
+    res = res + '('
+    la = lookahead(la[1])
+
+    while True:
+      if la[0] == 'VOID':
+        break
+
+      res = res + la[0]
+      la = lookahead(la[1])
+
+      if la[0] == 'AS':
+        break
+      else:
+        res = res + ','
+
+    res = res + '):'
+    return res
+  else:
+    raise SyntaxError('improper function declaration')
+
+def parse_call(tokens):
+  
+  res = ""
+
+  la = lookahead(tokens)
+
+  res = res + la[0]
+
+  la = lookahead(la[1])
+
+  if la[0] == 
+
 
 def parse_string(tokens):
 
@@ -78,6 +127,10 @@ def parse_expression(tokens):
       tokens = ps[1]
     elif la[0] == "CALL":
       pass
+    elif la[0] == "VARIABLE":
+      pv = parse_variable(la[1])
+      res = res + pv[0]
+      tokens = pv[1]
     else:
       raise SyntaxError('invalid expression')
 
@@ -99,6 +152,18 @@ def parse(tokens):
 
       print_statement = parse_print(la[1])
       line = line + print_statement
+      res = res + line + '\n'
+
+    elif la[0] == "SET":
+
+      assign_statement = parse_assign(la[1])
+      line = line + assign_statement
+      res = res + line + '\n'
+
+    elif la[0] == "MAKE":
+
+      function_statement = parse_function(la[1])
+      line = line + function_statement
       res = res + line + '\n'
 
   return res
